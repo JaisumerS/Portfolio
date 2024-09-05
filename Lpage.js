32e2f440c2a58735386d0855583363ce47bf3679
JaisumerS <sumer.sandhu@hotmail.com>
2024-09-05 04:39:37 -0600

    Added about me section

--- Lpage.js
+++ Lpage.js
@@ -1,3 +1,15 @@
-document.querySelector('.blackholepng').addEventListener('click', function() {
-    document.querySelector('.appearingtext').classList.add('at');
-  });
+const blackhole = document.getElementById("blackholeclicker")
+const aboutme = document.getElementsByClassName("appearingtext")[0];
+let clicked = false;
+
+blackhole.addEventListener('click', e => {
+    if (!clicked) {
+        aboutme.classList.add('spawncard');
+        aboutme.classList.remove('spawnoutcard');
+        clicked = true;
+    } else {
+        aboutme.classList.remove('spawncard');
+        aboutme.classList.add('spawnoutcard');
+        clicked = false;
+    }
+});
