8d71c0c9940af22e0c4d1e41d3bb42903c47cf62
JaisumerS <sumer.sandhu@hotmail.com>
2024-09-08 03:54:56 -0600

    Added ring spawner

--- Lpage.js
+++ Lpage.js
@@ -1,16 +1,21 @@
 const blackhole = document.getElementById("blackholeclicker")
 const aboutme = document.getElementsByClassName("appearingtext")[0];
-const spinnerbanner = document.get
+const spinnerbanner = document.getElementById("objectspinner");
+
 let clicked = false;
 
 blackhole.addEventListener('click', e => {
     if (!clicked) {
         aboutme.classList.add('spawncard');
         aboutme.classList.remove('spawnoutcard');
+        spinnerbanner.style.pointerEvents = 'none';
+        spinnerbanner.style.opacity = '1';
         clicked = true;
     } else {
         aboutme.classList.remove('spawncard');
         aboutme.classList.add('spawnoutcard');
+        spinnerbanner.style.removeProperty('pointer-events');
+        spinnerbanner.style.opacity = '0';
         clicked = false;
     }
 });
