b79ab670317488dcd3bb7c7698ee098ab4a6cc7d
JaisumerS <sumer.sandhu@hotmail.com>
2024-09-08 05:17:49 -0600

    Finally fixed keyframe issue

--- Lpage.js
+++ Lpage.js
@@ -2,6 +2,7 @@ const blackhole = document.getElementById("blackholeclicker")
 const aboutme = document.getElementsByClassName("appearingtext")[0];
 const spinnerBanner = document.getElementById("objectspinner");
 let clicked = false;
+let animForward = true;
 
 const ringKeyframes = [
     {bottom: '0%', opacity: 0},
@@ -22,13 +23,18 @@ blackhole.addEventListener('click', e => {
         aboutme.classList.add('spawncard');
         aboutme.classList.remove('spawnoutcard');
         spinnerBanner.style.pointerEvents = 'none';/*temporary*/
-        ringSpawn.play();
+        if (animForward) {
+            ringSpawn.play()
+        } else {
+            ringSpawn.reverse()
+        }
         clicked = true;
     } else {
         aboutme.classList.remove('spawncard');
         aboutme.classList.add('spawnoutcard');
         spinnerBanner.style.removeProperty('pointer-events');/*temporary*/
         ringSpawn.reverse();
+        animForward = false;
         clicked = false;
     }
 });
